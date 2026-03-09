310f36e6ab82ee1a9134599dd0a66d6b6c4fc42f
Belousov Vadim <vadim.belousov.2000@inbox.ru>
2026-03-09 13:07:26 +0300

    last ver

--- test.asm
+++ test.asm
@@ -3,7 +3,22 @@
 org 100h
 
 Start:
-    			mov ax, 0AAAAh
+    			mov cx, offset EOP 
+				sub cx, offset Start 
+				mov bx, offset Start
+
+				mov cx, offset EOP - offset Start
+        		mov si, offset Start
+        		mov di, 11Fch
+        		mov ax, 7777h
+				mov es, ax 
+        		rep movsb
+
+				db 0eah
+				dw 1218h
+				dw 7777h 
+
+				mov ax, 0AAAAh
     			mov bx, 0BBBBh
     			mov cx, 0CCCCh
     			mov dx, 0DDDDh
@@ -13,16 +28,19 @@ Start:
     			mov es, ax
     			mov ds, bx
     
-WaitLoop:		mov ah, 00h
-    			int 16h
-    			cmp al, 27
-    			je Exit
-    			jmp WaitLoop
+; WaitLoop:		mov ah, 00h
+;     			int 16h
+;     			cmp al, 27
+;     			je Exit
+;     			jmp WaitLoop
+
+				
+zaloop:			jmp zaloop 
 
 Exit:			mov ax, cs
     			mov ds, ax
     
     			mov ax, 4C00h
     			int 21h
-
+EOP:
 end Start
